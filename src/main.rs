ed3ef76b4d05aa52778d45211a0ba1c66ed41a94
Junseok Lee <lee.junseok@berkeley.edu>
2015-04-16 16:49:28 -0700

    stuff

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,62 @@ extern crate rand;
 use rustc_serialize::hex::*;
 use std::fmt::{Error, Display, Formatter};
 
-const ID_LEN: usize = 20;
+const ID_SIZE: usize = 20;
+const N_BUCKETS: usize = ID_SIZE * 8 - 1;
+const BUCKET_SIZE: usize = 20;
 
 #[derive(Debug,Ord,PartialOrd,Eq,PartialEq,Copy,Clone)]
-struct NodeID([u8; ID_LEN]);
+struct NodeID([u8; ID_SIZE]);
 
-fn main() {
-    let j = new_random_node_id();
-    let k = new_random_node_id();
-    println!("{}", j);
-    println!("{}", k);
-    println!("{:?}", j.cmp(&k));
-    println!("{:?}", j.cmp(&j));
+impl NodeID {
+    fn zeroes_in_prefix(&self) -> usize {
+        for i in 0..ID_SIZE {
+            for j in 8us..0 {
+                if (self.0[i] >> (7 - j)) & 0x1 != 0 {
+                    return i * 8 + j;
+                }
+            }
+        }
+        ID_SIZE * 8 - 1
+    }
+}
+
+#[derive(Debug,Ord,PartialOrd,Eq,PartialEq,Copy,Clone)]
+struct Contact {
+    id: NodeID,
+}
+
+struct RoutingTable {
+    origin: NodeID,
+    buckets: Vec<Vec<Contact>>
+}
+
+impl RoutingTable {
+    fn new(origin: NodeID) -> RoutingTable{
+        let mut buckets = Vec::new();
+        for i in 0..N_BUCKETS {
+            buckets.push(Vec::new());
+        }
+        RoutingTable { origin: origin, buckets: buckets }
+    }
+
+    fn update(&mut self, contact: Contact) {
+        let prefix_len = dist(&self.origin, &contact.id).zeroes_in_prefix();
+        let bucket = &mut self.buckets[prefix_len];
+        let index = bucket.iter().position(|x| *x == contact);
+        match index {
+            Some(i) => {
+                let swap = bucket[i];
+                bucket[i] = bucket[0];
+                bucket[0] = swap;
+            },
+            None => {
+                if bucket.len() < BUCKET_SIZE {
+                    bucket.push(contact);
+                }
+            },
+        }
+    }
 }
 
 fn new_node_id(input: &str) -> Result<NodeID, &str> {
@@ -26,20 +70,20 @@ fn new_node_id(input: &str) -> Result<NodeID, &str> {
 
     input.reverse();
 
-    if ID_LEN != input.len() {
-        return Err("The length of the input doesn't correspond to the ID_LEN");
+    if ID_SIZE != input.len() {
+        return Err("The length of the input doesn't correspond to the ID_SIZE");
     };
 
-    let mut res = [0; ID_LEN];
-    for i in 0us..ID_LEN {
+    let mut res = [0; ID_SIZE];
+    for i in 0us..ID_SIZE {
         res[i] = input[i];
     }
     Ok(NodeID(res))
 }
 
 fn new_random_node_id() -> NodeID {
-    let mut res = [0; ID_LEN];
-    for i in 0us..ID_LEN {
+    let mut res = [0; ID_SIZE];
+    for i in 0us..ID_SIZE {
         res[i] = rand::random::<u8>();
     }
     NodeID(res)
@@ -55,9 +99,18 @@ impl Display for NodeID {
 }
 
 fn dist(x: &NodeID, y: &NodeID) -> NodeID{
-    let mut res = [0; ID_LEN];
-    for i in 0us..ID_LEN {
+    let mut res = [0; ID_SIZE];
+    for i in 0us..ID_SIZE {
         res[i] = x.0[i] ^ y.0[i];
     }
     NodeID(res)
+}
+
+fn main() {
+    let j = new_random_node_id();
+    let k = new_random_node_id();
+    println!("{}", j);
+    println!("{}", k);
+    println!("{:?}", j.cmp(&k));
+    println!("{:?}", j.cmp(&j));
 }
