f801d343549f4c335c6aa41ad31ad478ce0bdfef
Junseok Lee <lee.junseok@berkeley.edu>
2015-04-21 11:24:34 -0700

    more fun main test

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ extern crate rustc_serialize;
 use kademlia::*;
 
 fn main() {
-    let handle = Kademlia::start("test_net", Key::random(), "127.0.0.1:0", "127.0.0.1:0");
+    let mut nodes = Vec::new();
+    for _ in 0..60 {
+        nodes.push(Kademlia::start("test_net", Key::random(), "127.0.0.1:0", "127.0.0.1:0"));
+    }
     loop {}
 }
